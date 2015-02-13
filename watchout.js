d6d128d0a2cf909db116ed37051bad0c07065174
dannydelott <dannydelott@gmail.com>
2015-02-13 11:56:54 -0800

    Add updateScoreboard

--- watchout.js
+++ watchout.js
@@ -1,2 +1,10 @@
 // start slingin' some d3 here.
 
+// update scores
+var updateScoreboard = function(scores){
+  d3.select('.scoreboard')
+    .selectAll('span')
+    .data(scores)
+    .text(function(d){ return d; });
+};
+
