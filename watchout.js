ca16980f21086ca20146b58dd69c06491d6674ef
dannydelott <dannydelott@gmail.com>
2015-02-13 12:23:57 -0800

    Add addCircle
    - Add a circle to the svg container at the passed in coordinates.

--- watchout.js
+++ watchout.js
@@ -1,10 +1,45 @@
-// start slingin' some d3 here.
+// adds the circle container
+var svgContainer = d3.select("body").append("svg")
+  .attr("width", 800)
+  .attr("height", 500);
 
-// update scores
+// updates scores
 var updateScoreboard = function(scores){
   d3.select('.scoreboard')
     .selectAll('span')
     .data(scores)
+    .transition()
     .text(function(d){ return d; });
 };
+
+// adds a circle to the svgContainer at the xy coordinates.
+var addCircle = function(x, y){
+  var circle_data = [{
+    "x_axis": x,
+    "y_axis": y,
+    "radius": 20,
+    "color": "purple"
+  }];
+
+  var circle = svgContainer.append("circle")
+    .data(circle_data);
+
+  var circleAttributes = circle
+    .attr("cx", function(d) {
+      return d.x_axis;
+    })
+    .attr("cy", function(d) {
+      return d.y_axis;
+    })
+    .attr("r", function(d) {
+      return d.radius;
+    })
+    .style("fill", function(d) {
+      return d.color;
+    });
+};
+
+addCircle(100,100);
+addCircle(200,200);
+addCircle(400,400);
 
